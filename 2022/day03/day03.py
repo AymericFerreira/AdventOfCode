513b255aafdcf6fda89ca744b1b276c5c475c5ec
AymericFerreira <aymeric.ferreira.1@ulaval.ca>
2022-12-03 11:53:26 -0500

    Update part 2, I was thinking it was a block of 6 Update part 1 to use function created for part 2

--- 2022/day03/day03.py
+++ 2022/day03/day03.py
@@ -9,10 +9,7 @@ def part_1(input: str):
         compartment1 = set(line[:len(line)//2])
         compartment2 = set(line[len(line)//2:])
         letter = compartment1.intersection(compartment2)
-        if list(letter)[0].isupper():
-            priority += ord(list(letter)[0]) - 64 + 26
-        else:
-            priority += ord(list(letter)[0]) - 96
+        priority += convert_to_priority(letter)
     return priority
 
 
@@ -27,14 +24,11 @@ def convert_to_priority(char: set):
 def part_2(input: str):
     lines = input.splitlines()
     priority = 0
-    for elf1, elf2, elf3, elf4, elf5, elf6 in \
-            zip(lines[::6], lines[1::6], lines[2::6], lines[3::6], lines[4::6], lines[5::6]):
-        compartiment1 = set(elf1).intersection(set(elf2)).intersection(set(elf3))
-        compartiment2 = set(elf4).intersection(set(elf5)).intersection(set(elf6))
-        priority += convert_to_priority(compartiment1)
-        priority += convert_to_priority(compartiment2)
+    for elf1, elf2, elf3 in \
+            zip(lines[::3], lines[1::3], lines[2::3]):
+        compartment = set(elf1).intersection(set(elf2)).intersection(set(elf3))
+        priority += convert_to_priority(compartment)
     return priority
-
 
 
 def submit_solution_part_1():
@@ -49,5 +43,6 @@ if __name__ == '__main__':
     input_content = pathlib.Path("input.txt").read_text()
     print(part_1(input_content))
     print(part_2(input_content))
+    print(part_2(input_content) == 2752)
     # submit_solution_part_1()
     # submit_solution_part_2()
